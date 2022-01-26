9fed2d0d88ce0b9eb64692ed381d4f15a3f22e0e
shuvo794 <shuvohossain794@gmail.com>
2022-01-26 10:14:11 +0600

    Start Travel Agency Server and set up mongoDB conncetion

--- index.js
+++ index.js
@@ -7,7 +7,14 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 const port = process.env.PORT || 5000;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}
+@cluster0.8g1rh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
+// console.log(uri);
 
 
 
