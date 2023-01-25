4ea7061c2b31a006eff195d654cc3d68820a61e6
shuvo794 <shuvohossain794@gmail.com>
2023-01-25 09:41:15 +0600

    try to clean code

--- index.js
+++ index.js
@@ -153,7 +153,7 @@ async function run() {
 }
 run().catch(console.dir);
 app.get("/", (req, res) => {
-  res.send("Hello Tour Bd!");
+  res.send("Hello Tour jone!");
 });
 
 app.listen(port, () => {
